2b147e0c43e2301337d32ac6134b85ee9fc42822
Laurence <94482980+ld17282@users.noreply.github.com>
2022-07-13 19:37:28 -0700

    progress as of 7/13 19:32 PST. completed conditional redering on nav bar based on login status

--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
 import LandingPage from "./components/LandingPage.jsx";
+import Profile from "./components/Profile.jsx";
 import "./styles.scss";
 
 const App = (props) => {
@@ -13,6 +14,7 @@ const App = (props) => {
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/profile" component={Profile} />
           </Switch>
         </Router>
       </main>
--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -3,6 +3,7 @@ import { Link, Redirect, useHistory } from "react-router-dom";
 import "../styles.scss";
 import axios from "axios";
 import logo from "../assets/blueParq.png";
+import profile from "../assets/profile.png";
 import topoBackground from "../assets/topoBackground.png";
 import bookArchway from "../assets/book archway.png";
 import hostArchway from "../assets/host archway.png";
@@ -41,6 +42,8 @@ export default function LandingPage() {
   const classes = useStyles();
 
   const [address, setAddress] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
   // const [data, setData] = useState({
   //   lat: 34.052235,
   //   lng: -118.243683,
@@ -49,6 +52,13 @@ export default function LandingPage() {
 
   // set hisstory to carry data during axios req
   let history = useHistory();
+
+  // check if there is a access_token in the session storage
+  const access_token = window.sessionStorage.getItem("access_token");
+
+  useEffect(() => {
+    setLoggedIn(access_token ? true : false);
+  }, []);
 
   const handleSubmit = (e) => {
     // prevent refresh of the screen
@@ -69,6 +79,7 @@ export default function LandingPage() {
       });
   };
 
+  
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <div className="navBar" style={{ height: "70px" }} sx={{ flexGrow: 1 }}>
@@ -119,7 +130,8 @@ export default function LandingPage() {
               </Typography>
             </Button>
             <Button color="inherit" sx={{ flexGrow: 1 }}>
-              <Typography
+              {loggedIn === false && (
+                <Typography
                 variant="h6"
                 component="div"
                 sx={{
@@ -130,6 +142,12 @@ export default function LandingPage() {
               >
                 <LoginPopup />
               </Typography>
+            )}
+            {loggedIn === true && (
+              <Link to='/profile'>
+                <img className="profile" src={profile}></img>
+              </Link>
+            )}
             </Button>
           </Toolbar>
         </Box>
