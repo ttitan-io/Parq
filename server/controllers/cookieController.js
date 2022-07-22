4c29a059b4ed3b2f89ab1620d88c10eba1361a4d
jerikko <jerikkoagatep@gmail.com>
2022-07-21 17:43:27 -0700

    Resolved conflicts

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -26,11 +26,7 @@ cookieController.setCookie = (req, res, next) => {
 //for FRONTEND: send token in Authorization header: `authorization: Bearer: ${accessToken}`
 
 cookieController.verifyCookie = (req, res, next) => {
-<<<<<<< HEAD
-  console.log('we got here in verifyCookie', req.headers)
-=======
   // console.log('Headers ==>', req.headers)
->>>>>>> dev
   const token = req.headers.authorization.split(" ")[1];
   console.log("token:", token);
   // if (token === null) {
