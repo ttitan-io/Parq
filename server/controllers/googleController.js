f3a635141f85b7c381231d72af9d0e33d817b1bb
jerikko <jerikkoagatep@gmail.com>
2022-07-21 17:44:11 -0700

    Resolved conflicts again

--- server/controllers/googleController.js
+++ server/controllers/googleController.js
@@ -14,11 +14,7 @@ googleRequestController.mapLocation = (req, res, next) => {
         params: {
           address: address,
           key: process.env.GOOGLE_API_KEY,
-<<<<<<< HEAD
-        }
-=======
         },
->>>>>>> dev
       })
       .then(response => {
         // console.log("response results [0]:", response.data.results[0]);
